12e02148435d6cb0e5381b5f5a9a74913fea223a
dolores <zsh199301@163.com>
2019-09-20 18:04:21 +0800

    注册

---
+++ src/js/caseView.js
@@ -0,0 +1,16 @@
+(function(){
+
+    var data = {coverImgUrl: '../img/1.png',
+        avatar:'../img/2.png',
+        name: '马云老师', 
+        edu: '启明教育', 
+        desc: '这里是大咖简介这里是大咖简介这里是大咖简介这里是大咖简介这里是介这里是大咖简介这里是大咖简介这里是大咖简介这里是大咖简介这里是大咖简介',
+        title: '带你解开企业管理的奥秘-基本管理法则',
+        date: 1568094793625,
+        detail: '在中国，重庆机场的人脸识别系统成功匹配到'
+    }
+
+    var html = template('tpl-case-view', data);
+    $('.container .wrapper').html(html);
+    
+}())
